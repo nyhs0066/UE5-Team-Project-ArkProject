04f2689b1d6602baf6112ba369ebbd4474a4d593
Canopola <108908343+nyhs0066@users.noreply.github.com>
2023-05-23 08:18:25 +0900

    초기 커밋.

---
+++ Source/ArkProject/THObject.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "THObject.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARKPROJECT_API UTHObject : public UObject
+{
+	GENERATED_BODY()
+	
+};
