0079e96e3d21e683236cc593cc930329cf7f81ed
ClumsyTurtleWany <kdw41424142@gmail.com>
2023-05-26 16:07:55 +0900

    Update User Interface Part
    유저 인터페이스 파트 구현중이나 일단 커밋 올립니다.

---
+++ Source/ArkProject/Public/ArkProjectGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "ArkProjectGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARKPROJECT_API UArkProjectGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
